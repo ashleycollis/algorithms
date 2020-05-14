08505fba61166d74fc2034c367a18312ea74f3f8
Ashley Collis <ashleycollisburgess@gmail.com>
2020-05-13 23:59:13 -0400

    python selection sort

--- algorithms.py
+++ algorithms.py
@@ -1,1 +1,31 @@
 def binary_search(list, item):
+    low = 0
+    high = len(list)-1
+    while (low <= high):
+        mid = low + high
+        guess = list[mid]
+        if guess == item
+        return mid
+        if guess < item
+        low = mid + 1
+        else
+        high = mid - 1
+    return None
+
+
+def smallestNumber(arr):
+    smallest = arr[0]
+    smallestIndex = 0
+    for num in range(1, len(arr)):
+        if arr[num] < smallest:
+            smallest = arr[num]
+            smallest_index = num
+    return smallest_index
+
+
+def selectionSort(arr):
+    newArr = []
+    for num in range(1, len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
